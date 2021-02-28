99416abd427db354d04cf2d129646b6c43aee983
Sai Sagar Seru <saisseru@gmail.com>
2021-02-28 21:36:31 +0530

    Done?

--- src/Feed.js
+++ src/Feed.js
@@ -3,7 +3,7 @@ import "./Feed.css"
 import Post from './Post'
 import TweetBox from "./TweetBox"
 import db from "./firebase"
-
+import FlipMove from "react-flip-move"
 
 function Feed() {
     const [posts, setPosts] = useState([])
@@ -23,17 +23,19 @@ function Feed() {
 
             {/* Tweet Box */}
             <TweetBox />
-
-            {posts.map(post => (
-                <Post 
-                displayName={post.displayName}
-                username={post.username}
-                verified={post.verified}
-                text={post.text}
-                avatar={post.avatar}
-                image={post.image}
-            />
-            ))}
+            <FlipMove>
+                {posts.map(post => (
+                    <Post 
+                        key={post.text}
+                        displayName={post.displayName}
+                        username={post.username}
+                        verified={post.verified}
+                        text={post.text}
+                        avatar={post.avatar}
+                        image={post.image}
+                    />
+                    ))}
+            </FlipMove>
 
             
         </div>
--- src/TweetBox.js
+++ src/TweetBox.js
@@ -1,21 +1,50 @@
 import { Avatar, Button } from '@material-ui/core'
-import React from 'react'
+import React, { useState } from 'react'
 import './TweetBox.css'
+import db from "./firebase"
+
 
 function TweetBox() {
+    const [tweetMessage, setTweetMessage] = useState("")
+    const [tweetImage, setTweetImage] = useState("")
+
+    const sendTweet = e => {
+        e.preventDefault();
+        db.collection("posts").add({
+            displayName: "Sai Sagar Seru",
+            username: "ssseru",
+            verified: true,
+            text: tweetMessage,
+            image: tweetImage,
+            avatar:
+              "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
+          });
+
+          setTweetMessage("")
+          setTweetImage("")
+    }
+
     return (
+
         <div className="tweetBox">
             <form>
                 <div className="tweetBox__input">
                     <Avatar src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg" />
-                    <input placeholder="What's happening?" type="text"/>
+                    <input 
+                        value={tweetMessage} 
+                        placeholder="What's happening?" 
+                        type="text"
+                        onChange={e => setTweetMessage(e.target.value)}
+                    />
                 </div>
                 <input 
+                value={tweetImage}
                     className="tweetBox__imageInput"
                     placeholder="Optional:: Enter Image URL"
                     type="text"
+                    onChange={e => setTweetImage(e.target.value)}
                 />
-                <Button className="tweetBox__tweetButton">Tweet</Button>
+                <Button onClick={sendTweet} type="submit" className="tweetBox__tweetButton">Tweet</Button>
             </form>
         </div>
     )
